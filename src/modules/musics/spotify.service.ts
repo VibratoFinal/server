62cb5ae78615c96c2d011d2f48973ee5023651ac
leedohyung28 <dhleee0123@gmail.com>
2024-09-25 15:03:57 +0900

    🚑Hotfix Error Log 표시

--- src/modules/musics/spotify.service.ts
+++ src/modules/musics/spotify.service.ts
@@ -24,104 +24,124 @@ export class SpotifyService {
   ) {}
 
   public async getAccessToken(): Promise<string> {
-    const url = "https://accounts.spotify.com/api/token";
-    const response = await axios.post(
-      url,
-      qs.stringify({ grant_type: "client_credentials" }),
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          Authorization:
-            "Basic " +
-            Buffer.from(
-              `${this.configService.get("SPOTIFY_CLIENT_ID")}:${this.configService.get("SPOTIFY_CLIENT_SECRET")}`,
-            ).toString("base64"),
+    try {
+      const url = "https://accounts.spotify.com/api/token";
+      const response = await axios.post(
+        url,
+        qs.stringify({ grant_type: "client_credentials" }),
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization:
+              "Basic " +
+              Buffer.from(
+                `${this.configService.get("SPOTIFY_CLIENT_ID")}:${this.configService.get("SPOTIFY_CLIENT_SECRET")}`,
+              ).toString("base64"),
+          },
         },
-      },
-    );
-    return response.data.access_token;
+      );
+      return response.data.access_token;
+    } catch (err) {
+      console.error("Failed to Get Spotify Access Token : ", err);
+    }
   }
 
   public async searchAll(body: string): Promise<SearchAllDTO> {
-    const accessToken = await this.getAccessToken();
-    const url = "https://api.spotify.com/v1/search";
+    try {
+      const accessToken = await this.getAccessToken();
+      const url = "https://api.spotify.com/v1/search";
 
-    const response = await axios.get(url, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-      params: {
-        q: body,
-        type: "track,artist,album",
-        market: "KR",
-        limit: "20",
-        offset: "5",
-      },
-    });
+      const response = await axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        params: {
+          q: body,
+          type: "track,artist,album",
+          market: "KR",
+          limit: "20",
+          offset: "5",
+        },
+      });
 
-    return transformAll(response.data);
+      return transformAll(response.data);
+    } catch (err) {
+      console.error("Failed to Search Spotify All : ", err);
+    }
   }
 
   public async searchTrack(body: string): Promise<TrackDTO[]> {
-    const accessToken = await this.getAccessToken();
-    const url = "https://api.spotify.com/v1/search";
+    try {
+      const accessToken = await this.getAccessToken();
+      const url = "https://api.spotify.com/v1/search";
 
-    const response = await axios.get(url, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-      params: {
-        q: body,
-        type: "track",
-        market: "KR",
-        limit: "20",
-        offset: "5",
-      },
-    });
+      const response = await axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        params: {
+          q: body,
+          type: "track",
+          market: "KR",
+          limit: "20",
+          offset: "5",
+        },
+      });
 
-    const tracks = transformTracks(response.data.tracks.items);
-    // const tracksInfoPromises = tracks.map(track =>
-    //   this.songsDetailService.getTracksInfo(track.id),
-    // );
-    // await Promise.all(tracksInfoPromises);
+      const tracks = transformTracks(response.data.tracks.items);
+      // const tracksInfoPromises = tracks.map(track =>
+      //   this.songsDetailService.getTracksInfo(track.id),
+      // );
+      // await Promise.all(tracksInfoPromises);
 
-    return tracks;
+      return tracks;
+    } catch (err) {
+      console.error("Failed to Search Spotify Track : ", err);
+    }
   }
 
   public async searchArtist(body: string): Promise<ArtistDTO[]> {
-    const accessToken = await this.getAccessToken();
-    const url = "https://api.spotify.com/v1/search";
+    try {
+      const accessToken = await this.getAccessToken();
+      const url = "https://api.spotify.com/v1/search";
 
-    const response = await axios.get(url, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-      params: {
-        q: body,
-        type: "artist",
-        market: "KR",
-        limit: "20",
-        offset: "5",
-      },
-    });
+      const response = await axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        params: {
+          q: body,
+          type: "artist",
+          market: "KR",
+          limit: "20",
+          offset: "5",
+        },
+      });
 
-    return transformArtists(response.data.artists.items);
+      return transformArtists(response.data.artists.items);
+    } catch (err) {
+      console.error("Failed to Search Spotify Artist : ", err);
+    }
   }
 
   public async searchAlbum(body: string): Promise<AlbumDTO[]> {
-    const accessToken = await this.getAccessToken();
-    const url = "https://api.spotify.com/v1/search";
+    try {
+      const accessToken = await this.getAccessToken();
+      const url = "https://api.spotify.com/v1/search";
 
-    const response = await axios.get(url, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-      params: {
-        q: body,
-        type: "album",
-      },
-    });
+      const response = await axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        params: {
+          q: body,
+          type: "album",
+        },
+      });
 
-    return transformAlbums(response.data.albums.items);
+      return transformAlbums(response.data.albums.items);
+    } catch (err) {
+      console.error("Failed to Search Spotify Album : ", err);
+    }
   }
 }
--- src/modules/searchs/searchs.service.ts
+++ src/modules/searchs/searchs.service.ts
@@ -13,41 +13,53 @@ export class SearchsService {
   constructor(private readonly spotifyService: SpotifyService) {}
 
   public async getArtist(artist_id: string): Promise<ArtistDTO> {
-    const accessToken = await this.spotifyService.getAccessToken();
+    try {
+      const accessToken = await this.spotifyService.getAccessToken();
 
-    const url = `https://api.spotify.com/v1/artists/${artist_id}`;
-    const response = await axios.get(url, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+      const url = `https://api.spotify.com/v1/artists/${artist_id}`;
+      const response = await axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
 
-    return transformArtist(response.data);
+      return transformArtist(response.data);
+    } catch (err) {
+      console.error("Failed to Get Artist by Id : ", err);
+    }
   }
 
   public async getAlbum(album_id: string): Promise<AlbumDTO> {
-    const accessToken = await this.spotifyService.getAccessToken();
+    try {
+      const accessToken = await this.spotifyService.getAccessToken();
 
-    const url = `https://api.spotify.com/v1/albums/${album_id}`;
-    const response = await axios.get(url, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+      const url = `https://api.spotify.com/v1/albums/${album_id}`;
+      const response = await axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
 
-    return transformAlbum(response.data);
+      return transformAlbum(response.data);
+    } catch (err) {
+      console.error("Failed to Get Album by Id : ", err);
+    }
   }
 
   public async getTrack(track_id: string): Promise<TrackDTO> {
-    const accessToken = await this.spotifyService.getAccessToken();
+    try {
+      const accessToken = await this.spotifyService.getAccessToken();
 
-    const url = `https://api.spotify.com/v1/tracks/${track_id}`;
-    const response = await axios.get(url, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+      const url = `https://api.spotify.com/v1/tracks/${track_id}`;
+      const response = await axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
 
-    return transformTrack(response.data);
+      return transformTrack(response.data);
+    } catch (err) {
+      console.error("Failed to Get Track by Id : ", err);
+    }
   }
 }
