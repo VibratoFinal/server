308e0a76d7b6a50b2b8a875aef995b85c8b4d41b
vancouver <kjh03291@gmail.com>
2024-10-01 18:58:06 +0900

    :bug: comments,reviews Test error 해결

--- src/modules/reviews/reviews.controller.spec.ts
+++ src/modules/reviews/reviews.controller.spec.ts
@@ -3,25 +3,32 @@ import { ReviewsController } from "./reviews.controller";
 import { ReviewsService } from "./reviews.service";
 import { getRepositoryToken } from "@nestjs/typeorm";
 import { Reviews } from "./entity/reviews.entity";
+import { Comments } from "../comments/entity/comments.entity";
+import { FirebaseService } from "@/configs/firebase/firebase.service";
 
-describe("ReivewsController", () => {
+describe("ReviewsController", () => {
   let controller: ReviewsController;
   let service: ReviewsService;
+  let firebaseService: FirebaseService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ReviewsController],
       providers: [
         ReviewsService,
+        { provide: FirebaseService, useValue: {} },
         { provide: getRepositoryToken(Reviews), useValue: {} },
+        { provide: getRepositoryToken(Comments), useValue: {} },
       ],
     }).compile();
 
     controller = module.get<ReviewsController>(ReviewsController);
     service = module.get<ReviewsService>(ReviewsService);
+    firebaseService = module.get<FirebaseService>(FirebaseService);
   });
 
   it("should be defined", () => {
     expect(controller).toBeDefined();
     expect(service).toBeDefined();
+    expect(firebaseService).toBeDefined();
   });
 });
--- src/modules/reviews/reviews.service.spec.ts
+++ src/modules/reviews/reviews.service.spec.ts
@@ -2,25 +2,29 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { ReviewsService } from "./reviews.service";
 import { getRepositoryToken } from "@nestjs/typeorm";
 import { Reviews } from "./entity/reviews.entity";
+import { Comments } from "../comments/entity/comments.entity";
+import { FirebaseService } from "@/configs/firebase/firebase.service";
 
 describe("ReivewsService", () => {
   let service: ReviewsService;
+  let firebaseService: FirebaseService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         ReviewsService,
-        {
-          provide: getRepositoryToken(Reviews),
-          useValue: {},
-        },
+        { provide: FirebaseService, useValue: {} },
+        { provide: getRepositoryToken(Reviews), useValue: {} },
+        { provide: getRepositoryToken(Comments), useValue: {} },
       ],
     }).compile();
 
     service = module.get<ReviewsService>(ReviewsService);
+    firebaseService = module.get<FirebaseService>(FirebaseService);
   });
 
   it("should be defined", () => {
     expect(service).toBeDefined();
+    expect(firebaseService).toBeDefined();
   });
 });
