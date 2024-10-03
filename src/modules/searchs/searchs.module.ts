dbba4bc3b98b15b02c63a0af4926faca44df6adf
leedohyung28 <dhleee0123@gmail.com>
2024-10-03 16:03:51 +0900

    💄Searchs Query, Param 적용

--- src/modules/searchs/searchs.module.ts
+++ src/modules/searchs/searchs.module.ts
@@ -2,10 +2,9 @@ import { Module } from "@nestjs/common";
 import { SearchsService } from "./searchs.service";
 import { SearchsController } from "./searchs.controller";
 import { SpotifyService } from "../musics/spotify.service";
-import { SongsDetailService } from "../musics/songsdetail.service";
 
 @Module({
-  providers: [SearchsService, SpotifyService, SongsDetailService],
+  providers: [SearchsService, SpotifyService],
   controllers: [SearchsController],
 })
 export class SearchsModule {}
