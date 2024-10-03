cdc2f4ad1ba3514f58640481bf7d4bf338b526c5
leedohyung28 <dhleee0123@gmail.com>
2024-10-03 16:03:33 +0900

    💄Music Query, Param 적용 및 SongsDetail 삭제

--- src/modules/musics/musics.controller.ts
+++ src/modules/musics/musics.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Param, Query } from "@nestjs/common";
 import { SearchDTO } from "./dto/search-spotify.dto";
 import {
   AlbumDTO,
@@ -13,10 +13,24 @@ import { SkipAuth } from "@/common/decorators/skip-auth.decorator";
 export class MusicsController {
   constructor(private readonly musicsService: MusicsService) {}
 
-  @Get("")
+  @Get("/all")
   @SkipAuth()
   async searchAll(@Body() body: SearchDTO): Promise<SearchAllDTO> {
     return this.musicsService.searchAll(body.search_content);
+  }
+
+  @Get("/all/:search_content")
+  @SkipAuth()
+  async searchAllByParam(
+    @Param("search_content") search_content: string,
+  ): Promise<SearchAllDTO> {
+    return this.musicsService.searchAll(search_content);
+  }
+
+  @Get("/all_query")
+  @SkipAuth()
+  async searchAllByQuery(@Query() query: SearchDTO): Promise<SearchAllDTO> {
+    return this.musicsService.searchAll(query.search_content);
   }
 
   @Get("/tracks")
@@ -25,15 +39,57 @@ export class MusicsController {
     return this.musicsService.searchTrack(body.search_content);
   }
 
+  @Get("/tracks/:search_content")
+  @SkipAuth()
+  async searchTrackByParam(
+    @Param("search_content") search_content: string,
+  ): Promise<TrackDTO[]> {
+    return this.musicsService.searchTrack(search_content);
+  }
+
+  @Get("/tracks_query")
+  @SkipAuth()
+  async searchTrackByQuery(@Query() query: SearchDTO): Promise<TrackDTO[]> {
+    return this.musicsService.searchTrack(query.search_content);
+  }
+
   @Get("/artists")
   @SkipAuth()
   async searchArtist(@Body() body: SearchDTO): Promise<ArtistDTO[]> {
     return this.musicsService.searchArtist(body.search_content);
   }
 
+  @Get("/artists/:search_content")
+  @SkipAuth()
+  async searchArtistByParam(
+    @Param("search_content") search_content: string,
+  ): Promise<ArtistDTO[]> {
+    return this.musicsService.searchArtist(search_content);
+  }
+
+  @Get("/artists_query")
+  @SkipAuth()
+  async searchArtistByQuery(@Query() query: SearchDTO): Promise<ArtistDTO[]> {
+    return this.musicsService.searchArtist(query.search_content);
+  }
+
   @Get("/albums")
   @SkipAuth()
   async searchAlbum(@Body() body: SearchDTO): Promise<AlbumDTO[]> {
     return this.musicsService.searchAlbum(body.search_content);
+  }
+
+  @Get("/albums/:search_content")
+  @SkipAuth()
+  async searchAlbumByParam(
+    @Param("search_content") search_content: string,
+  ): Promise<AlbumDTO[]> {
+    return this.musicsService.searchAlbum(search_content);
+  }
+
+  @Get("/albums_query")
+  @SkipAuth()
+  async searchAlbumByQuery(@Query() query: SearchDTO): Promise<AlbumDTO[]> {
+    return this.musicsService.searchAlbum(query.search_content);
   }
 }
