f06bf9807e3cb647606d4921def719d170b99939
vancouver <kjh03291@gmail.com>
2024-10-08 15:03:06 +0900

    :ambulance: HOTFIX - port 3000->8080

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new LoggingInterceptor());
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
