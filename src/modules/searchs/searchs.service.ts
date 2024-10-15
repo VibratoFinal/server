764614cd2b7dec78a3b5ec2b8720385cf3410a78
leedohyung28 <dhleee0123@gmail.com>
2024-10-15 18:52:56 +0900

    ⚡트랙의 앨범의 나머지 트랙 출력

--- src/modules/searchs/searchs.service.ts
+++ src/modules/searchs/searchs.service.ts
@@ -11,6 +11,7 @@ export class SearchsService {
     private readonly searchRepository: SearchsRepository,
   ) {}
 
+  // ID를 통한 아티스트 검색
   public async getArtist(uid: string, artist_id: string): Promise<ArtistDTO> {
     try {
       const accessToken = await this.spotifyService.getAccessToken();
@@ -40,6 +41,7 @@ export class SearchsService {
     }
   }
 
+  // ID를 통한 앨범 검색
   public async getAlbum(uid: string, album_id: string): Promise<AlbumDTO> {
     try {
       const accessToken = await this.spotifyService.getAccessToken();
@@ -69,6 +71,7 @@ export class SearchsService {
     }
   }
 
+  // ID를 통한 트랙 검색
   public async getTrack(uid: string, track_id: string): Promise<TrackDTO> {
     try {
       const accessToken = await this.spotifyService.getAccessToken();
@@ -91,6 +94,47 @@ export class SearchsService {
         {
           status: HttpStatus.INTERNAL_SERVER_ERROR,
           error: "Failed to get track by id",
+          details: err.message,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  // 트랙의 앨범 내 다른 트랙 출력
+  public async getRestTracksInAlbums(
+    uid: string,
+    track_id: string,
+  ): Promise<TrackDTO[]> {
+    try {
+      const accessToken = await this.spotifyService.getAccessToken();
+
+      const url = `https://api.spotify.com/v1/tracks/${track_id}`;
+      const response = await axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      const album_id = response.data.album.id;
+
+      const tracks_url = `https://api.spotify.com/v1/albums/${album_id}/tracks`;
+      const tracks_response = await axios.get(tracks_url, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      return await this.searchRepository.transformRestTracks(
+        uid,
+        tracks_response.data.items,
+      );
+    } catch (err) {
+      console.error("Failed to Get Rest Track from Albums : ", err);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: "Failed to get Rest Track from Albums",
           details: err.message,
         },
         HttpStatus.INTERNAL_SERVER_ERROR,
